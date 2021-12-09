2c7681c4682f60800a30854fba783b78c5dc1ae4
minhchuongyt2000 <minhchuongyt2000@gmail.com>
2021-12-09 06:47:47 +0000

    add name age to user

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,10 @@ ActiveRecord::Schema.define(version: 2021_12_09_060829) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
